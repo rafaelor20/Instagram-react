48118658cf8e9cb66c2eccd3c3535ebc0f361aa7
Rafael Oliveira Rosário <rafaelor20@hotmail.com>
2022-11-19 11:24:05 -0300

    troca de foto do usuario implementada

--- src/usuario.js
+++ src/usuario.js
@@ -2,9 +2,10 @@ import React from "react"
 
 export default function Usuario(props) {
     const [nome, setNome] = React.useState(props.usuario.nome)
+    const [foto, setFoto] = React.useState("assets/img/catanacomics.svg")
     return (
         <div className="usuario">
-            <img src="assets/img/catanacomics.svg" />
+            <img onClick={()=>setFoto(prompt("Ponha aqui o link da sua foto"))} src={foto} />
             <div className="texto">
                 <strong>catanacomics</strong>
                 <span onClick={()=>setNome(prompt("Qual o seu novo nome?"))}>
